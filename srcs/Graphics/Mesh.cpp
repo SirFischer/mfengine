806069fb928e626c287d43a5e1b1364ec6b0d7f1
Marek Fischer <marek5886@hotmail.com>
2020-03-22 12:35:16 +0100

    added texture mipmapping

--- srcs/Graphics/Mesh.cpp
+++ srcs/Graphics/Mesh.cpp
@@ -217,7 +217,7 @@ void		Mesh::SetTexture(sf::Image tImage)
 
 	glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_WRAP_S, GL_REPEAT);
 	glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_WRAP_T, GL_REPEAT);
-    glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_MIN_FILTER, GL_LINEAR);
+    glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_MIN_FILTER, GL_LINEAR_MIPMAP_LINEAR);
     glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_MAG_FILTER, GL_LINEAR);
 
 	glTexImage2D(GL_TEXTURE_2D, 0, GL_RGBA, tImage.getSize().x,tImage.getSize().y, 0, GL_RGBA, GL_UNSIGNED_BYTE, tImage.getPixelsPtr());
